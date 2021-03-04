2116e112dd9d209053e189678d3811806a3ab9ed
FUJISHIGE TEMMA <tenma.x0@gmail.com>
2021-03-04 15:12:02 +0900

    :rocket:step6

--- ip.c
+++ ip.c
@@ -24,6 +24,9 @@ struct ip_hdr
 const ip_addr_t IP_ADDR_ANY = 0x00000000;       /* 0.0.0.0 */
 const ip_addr_t IP_ADDR_BROADCAST = 0xffffffff; /* 255.255.255.255 */
 
+/* NOTE: if you want to add/delete the entries after net_run(), you need to protect these lists with a mutex. */
+static struct ip_iface *ifaces;
+
 int ip_addr_pton(const char *p, ip_addr_t *n)
 {
   char *sp, *ep;
@@ -95,12 +98,82 @@ void ip_dump(const uint8_t *data, size_t len)
   funlockfile(stderr);
 }
 
+struct ip_iface *
+ip_iface_alloc(const char *unicast, const char *netmask)
+{
+  struct ip_iface *iface;
+
+  iface = calloc(1, sizeof(*iface));
+  if (!iface)
+  {
+    errorf("calloc() failed");
+    return NULL;
+  }
+  NET_IFACE(iface)->family = NET_IFACE_FAMILY_IP;
+
+  if (ip_addr_pton(unicast, &iface->unicast) == -1)
+  {
+    errorf("ip_addr_pton(unicast) failed, addr=%s", unicast);
+    free(iface);
+    return NULL;
+  }
+  if (ip_addr_pton(netmask, &iface->netmask) == -1)
+  {
+    errorf("ip_addr_pton(netmask) failed, addr=%s", netmask);
+    free(iface);
+    return NULL;
+  }
+  iface->broadcast = (iface->unicast & iface->netmask) | ~iface->netmask;
+  return iface;
+}
+
+/* NOTE: must not be call after net_run() */
+int ip_iface_register(struct net_device *dev, struct ip_iface *iface)
+{
+  char unicast_addr[IP_ADDR_STR_LEN];
+  char netmask[IP_ADDR_STR_LEN];
+  char broadcast_addr[IP_ADDR_STR_LEN];
+
+  if (net_device_add_iface(dev, NET_IFACE(iface)) == -1)
+  {
+    errorf("net_device_add_iface() failed");
+    return -1;
+  }
+
+  iface->next = ifaces;
+  ifaces = iface;
+
+  infof("registerd: dev=%s, unicast=%s, netmask=%s, broadcast=%s", dev->name,
+        ip_addr_ntop(iface->unicast, unicast_addr, sizeof(unicast_addr)),
+        ip_addr_ntop(iface->netmask, netmask, sizeof(netmask)),
+        ip_addr_ntop(iface->broadcast, broadcast_addr, sizeof(broadcast_addr)));
+
+  return 0;
+}
+
+struct ip_iface *
+ip_iface_select(ip_addr_t addr)
+{
+  struct ip_iface *entry;
+
+  for (entry = ifaces; entry; entry = entry->next)
+  {
+    if (entry->unicast == addr)
+    {
+      break;
+    }
+  }
+  return entry;
+}
+
 static void
 ip_input(const uint8_t *data, size_t len, struct net_device *dev)
 {
   struct ip_hdr *hdr;
+  struct ip_iface *iface;
   uint8_t v;
   uint16_t hlen, total, offset;
+  char addr[IP_ADDR_STR_LEN];
 
   if (len < IP_HDR_SIZE_MIN)
   {
@@ -138,7 +211,23 @@ ip_input(const uint8_t *data, size_t len, struct net_device *dev)
     errorf("fragmentation is not supported");
     return;
   }
-  debugf("dev=%s, protocol=%u, total=%u", dev->name, hdr->protocol, total);
+
+  iface = (struct ip_iface *)net_device_get_iface(dev, NET_IFACE_FAMILY_IP);
+  if (!iface)
+  {
+    errorf("IP iface is not registered");
+    return;
+  }
+
+  if (hdr->dst != iface->unicast)
+  {
+    if (hdr->dst != iface->broadcast && hdr->dst != IP_ADDR_BROADCAST)
+    {
+      return;
+    }
+  }
+
+  debugf("dev=%s, iface=%s, protocol=%u, total=%u", dev->name, ip_addr_ntop(iface->unicast, addr, sizeof(addr)), hdr->protocol, total);
   ip_dump(data, total);
 }
 
